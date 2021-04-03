0eceeee4f64eae02c25b2e938060bfdcac945bfb
chestnut3108 <shivam31august1996@gmail.com>
2021-04-03 21:03:46 +0530

    WIP

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ parkingLot = None
 
 def createParkingLot(lot_capacity):
     global parkingLot
-    parkingLotRegistry =  ParkingLotRegistry()
+    parkingLotRegistry  =  ParkingLotRegistry()
     parkingLot = parkingLotRegistry.getParkingLot(int(lot_capacity))
 
 
@@ -24,27 +24,29 @@ def execute_command(command):
     command = command.strip("\n")
     command = command.split(' ')
     print("command Started", command)
+
     if command[0] == 'Create_parking_lot':
         createParkingLot(command[1])
 
     elif command[0] == 'Leave' and parkingLot is not None:
-
         parkingLot.leave(command[1])
 
-    elif command[0] == 'Park'and parkingLot is not None:
-
+    elif command[0] == 'Park' and parkingLot is not None:
         parkingLot.park(command[1], command[3])
-    elif command[0] == 'Slot_number_for_car_with_number'and parkingLot is not None:
-        
-        print(parkingLot.slotNumberByRegistrationNumber(command[1]))
-    elif command[0] == 'Slot_numbers_for_driver_of_age'and parkingLot is not None:
 
-        print(parkingLot.getSlotNumbersForDriverOfAge(command[1]))
+    elif command[0] == 'Slot_number_for_car_with_number' and parkingLot is not None:
+        print(parkingLot.slotNumberByRegistrationNumber(command[1]))
+
+    elif command[0] == 'Slot_numbers_for_driver_of_age' and parkingLot is not None:
+        result = parkingLot.getSlotNumbersForDriverOfAge(command[1])
+        if len(result) > 0:
+            print(', '.join(map(str, result)))
+        else:
+            print("No slot is booked by any user with age {}".format( command[1]))
 
     elif command[0] == 'Vehicle_registration_number_for_driver_of_age' and parkingLot is not None:
-
         result = parkingLot.getVehicleRegistrationNumbersByDriverAge(command[1])
-        if len(result)>0:
+        if len(result) > 0:
             print(', '.join(map(str, result)))
         else:
             print("No slot is booked by any user with age {}".format( command[1]))
